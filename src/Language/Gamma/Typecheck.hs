agrif/gammac

commit f570ed158c11bf5c5672a9fd02e487550b1482d7

implement GammaTypes and GammaAnnotated in terms of Data.Data

src/Language/Gamma/Typecheck.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies, TupleSections, TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies, TupleSections, TemplateHaskell, DeriveDataTypeable, DefaultSignatures #-}
 
 module Language.Gamma.Typecheck where
 
@@ -9,6 +9,8 @@ import Control.Monad.Except
 import Control.Monad.Writer
 import Control.Lens
 import Control.Lens.TH
+import Data.Data
+import Data.Data.Lens
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
@@ -23,7 +25,7 @@ declareLenses [d|
       , bindings :: Map GammaSym (GammaType ann)
       , substitutions :: Map Int (GammaType ann)
       , constraints :: [(ann, GammaSym, [GammaType ann])]
-      } deriving (Show)
+      } deriving (Show, Data)
   |]
 
 data TypError a = UnboundSymbol a GammaSym
@@ -70,9 +72,6 @@ lookupSym a name = do
 bindSym :: (Monad m) => GammaSym -> GammaType a -> TypT a m (GammaType a)
 bindSym name ty = ty <$ (bindings %= Map.insert name ty)
 
-class GammaTypes t a | t -> a where
-    types :: Traversal' t (GammaType a)
-
 mapType :: (GammaTypes t a) => Int -> GammaType a -> t -> t
 mapType n ty orig = orig & types %~ replace
     where replace t@(FreeType a m) | m == n    = ty
@@ -82,22 +81,16 @@ mapType n ty orig = orig & types %~ replace
 freeTypeVars :: (GammaTypes t a) => t -> Set Int
 freeTypeVars ty = ty ^. types._FreeType._2.to Set.singleton
 
-instance GammaTypes (GammaType a) a where
-    types f (FunType a fun args) = FunType a <$> types f fun <*> traverse (types f) args
-    types f (UnivType a t) = UnivType a <$> types f t
-    types f (ConstrainedType a name args t) = ConstrainedType a name <$> traverse (types f) args <*> types f t
-    types f t = f t
+class GammaTypes t a | t -> a where
+    types :: Traversal' t (GammaType a)
+    default types :: (Data t, Data a) => Traversal' t (GammaType a)
+    types = template
 
-instance GammaTypes (Map k (GammaType a)) a where
-    types = traverse.types
+instance (Data a) => GammaTypes (GammaType a) a
+instance (Data a) => GammaTypes (Map GammaSym (GammaType a)) a
+instance (Data a) => GammaTypes (TypState a) a
 
-instance GammaTypes [(a, GammaSym, [GammaType a])] a where
-    types = traverse._3.traverse.types
-
-instance GammaTypes (TypState a) a where
-    types f (TypState x bind subst const) = TypState x <$> types f bind <*> types f subst <*> types f const
-
-elimTyp :: (Monad m) => Int -> GammaType a -> TypT a m (GammaType a)
+elimTyp :: (Data a, Monad m) => Int -> GammaType a -> TypT a m (GammaType a)
 elimTyp n ty = do
   modify (mapType n ty)
   substitutions %= Map.insert n ty
@@ -107,7 +100,7 @@ constrainTyp :: (Monad m) => a -> GammaSym -> [GammaType a] -> TypT a m ()
 constrainTyp a name tys = constraints %= ((a, name, tys) :)
 
 class (Traversable (t a)) => GammaTypeable t a where
-    typecheck :: (Monad m) => t a (a, Int) -> TypT a m (GammaType a)
+    typecheck :: (Data a, Monad m) => t a (a, Int) -> TypT a m (GammaType a)
 
 instantiate :: GammaType a -> GammaType a -> GammaType a
 instantiate templ v = go 0 templ
@@ -118,7 +111,7 @@ instantiate templ v = go 0 templ
                                  | otherwise = t
           go n t = t
 
-abstract :: GammaType a -> Int -> GammaType a
+abstract :: (Data a) => GammaType a -> Int -> GammaType a
 abstract templ n | Set.member n (freeTypeVars templ) = UnivType (templ ^. annotation) (go 0 templ)
                  | otherwise = templ
    where go i (FunType a x ys) = FunType a (go i x) (fmap (go i) ys)
@@ -128,7 +121,7 @@ abstract templ n | Set.member n (freeTypeVars templ) = UnivType (templ ^. annota
                                | otherwise = t
          go i t = t
 
-unify :: (Monad m) => GammaType a -> GammaType a -> TypT a m (GammaType a)
+unify :: (Data a, Monad m) => GammaType a -> GammaType a -> TypT a m (GammaType a)
 unify ty@(FreeType a n) (FreeType b m)
     | n == m    = return ty
     | otherwise = elimTyp m ty
@@ -155,7 +148,7 @@ unify ty1@(PrimType _ p1) ty2@(PrimType _ p2) | p1 == p2 = return ty1
                                               | otherwise = throwError (CannotUnify ty1 ty2)
 unify ty1 ty2 = throwError (CannotUnify ty1 ty2)
 
-generalize :: (Monad m) => TypT a m (GammaType a) -> TypT a m (GammaType a)
+generalize :: (Data a, Monad m) => TypT a m (GammaType a) -> TypT a m (GammaType a)
 generalize m = do
   oldc <- use constraints
   constraints .= []
@@ -212,7 +205,7 @@ instance GammaTypeable GammaExpr a where
            FunType _ rty _ <- unify fty (FunType a ret argty)
            elimTyp i rty
 
-inferTypes :: (Monad m, GammaTypeable t a) => t a a -> TypT a m (t a (a, GammaType a))
+inferTypes :: (Data a, Monad m, GammaTypeable t a) => t a a -> TypT a m (t a (a, GammaType a))
 inferTypes code = do
     annotated <- (traverse (\a -> (a,) <$> createVar') code)
     typecheck annotated

src/Language/Gamma/Types.hs
@@ -1,54 +1,26 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable, FlexibleInstances, TemplateHaskell #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable, FlexibleInstances, TemplateHaskell, DeriveDataTypeable, MultiParamTypeClasses, FunctionalDependencies, DefaultSignatures #-}
 
 module Language.Gamma.Types where
 
 import Control.Lens
 import Control.Lens.TH
-
-class GammaAnnotated t where
-    annotation :: Lens' (t a) a
+import Data.Data
+import Data.Data.Lens
 
 type GammaSym = String
 
-class (HasGammaSym a) where
-    gammaSym :: Lens' a GammaSym
-
-instance (HasGammaSym GammaSym) where
-    gammaSym = id
-
 data GammaDecl t a = VarDecl a (GammaBind t a) (GammaExpr t a)
                    | FunDecl a GammaSym [GammaBind t a] (Maybe (GammaType t)) [GammaStmt t a]
-                     deriving (Show, Functor, Foldable, Traversable)
-
-instance GammaAnnotated (GammaDecl t) where
-    annotation f (VarDecl a x y) = f a <&> \b -> VarDecl b x y
-    annotation f (FunDecl a x y z w) = f a <&> \b -> FunDecl b x y z w
-
-instance HasGammaSym (GammaDecl t a) where
-    gammaSym f (VarDecl a x y) = gammaSym f x <&> \x' -> VarDecl a x' y
-    gammaSym f (FunDecl a x y z w) = gammaSym f x <&> \x' -> FunDecl a x' y z w
+                     deriving (Show, Functor, Foldable, Traversable, Data)
 
 data GammaBind t a = PlainBind a GammaSym
                    | TypeBind a GammaSym (GammaType t)
-                     deriving (Show, Functor, Foldable, Traversable)
-
-instance GammaAnnotated (GammaBind t) where
-    annotation f (PlainBind a x) = f a <&> \b -> PlainBind b x
-    annotation f (TypeBind a x y) = f a <&> \b -> TypeBind b x y
-
-instance HasGammaSym (GammaBind t a) where
-    gammaSym f (PlainBind a x) = gammaSym f x <&> \x' -> PlainBind a x'
-    gammaSym f (TypeBind a x y) = gammaSym f x <&> \x' -> TypeBind a x' y
+                     deriving (Show, Functor, Foldable, Traversable, Data)
 
 data GammaStmt t a = DeclStmt a (GammaDecl t a)
                    | ExprStmt a (GammaExpr t a)
                    | RetStmt a (GammaExpr t a)
-                     deriving (Show, Functor, Foldable, Traversable)
-
-instance GammaAnnotated (GammaStmt t) where
-    annotation f (DeclStmt a x) = f a <&> \b -> DeclStmt b x
-    annotation f (ExprStmt a x) = f a <&> \b -> ExprStmt b x
-    annotation f (RetStmt a x) = f a <&> \b -> RetStmt b x
+                     deriving (Show, Functor, Foldable, Traversable, Data)
 
 data GammaType a = PrimType a GammaPrimType
                  | FunType a (GammaType a) [GammaType a]
@@ -56,33 +28,19 @@ data GammaType a = PrimType a GammaPrimType
                  | UnivType a (GammaType a) -- de bruijen indexing
                  | BoundType a Int
                  | ConstrainedType a GammaSym [GammaType a] (GammaType a)
-                   deriving (Show, Functor)
-
-instance GammaAnnotated GammaType where
-    annotation f (PrimType a x) = f a <&> \b -> PrimType b x
-    annotation f (FunType a x y) = f a <&> \b -> FunType b x y
-    annotation f (FreeType a x) = f a <&> \b -> FreeType b x
-    annotation f (UnivType a x) = f a <&> \b -> UnivType b x
-    annotation f (BoundType a x) = f a <&> \b -> BoundType b x
-    annotation f (ConstrainedType a x y z) = f a <&> \b -> ConstrainedType b x y z
+                   deriving (Show, Functor, Data)
 
 data GammaPrimType = CInt
-                     deriving (Show, Eq)
+                     deriving (Show, Eq, Data)
 
 data GammaExpr t a = LitExpr a GammaLit
                    | SymExpr a GammaSym
                    | TypeExpr a (GammaExpr t a) (GammaType t)
                    | ApplyExpr a (GammaExpr t a) [GammaExpr t a]
-                     deriving (Show, Functor, Foldable, Traversable)
-
-instance GammaAnnotated (GammaExpr t) where
-    annotation f (LitExpr a x) = f a <&> \b -> LitExpr b x
-    annotation f (SymExpr a x) = f a <&> \b -> SymExpr b x
-    annotation f (TypeExpr a x y) = f a <&> \b -> TypeExpr b x y
-    annotation f (ApplyExpr a x y) = f a <&> \b -> ApplyExpr b x y
+                     deriving (Show, Functor, Foldable, Traversable, Data)
 
 data GammaLit = IntLit Integer
-                deriving (Show)
+                deriving (Show, Data)
 
 makePrisms ''GammaDecl
 makePrisms ''GammaBind
@@ -91,3 +49,29 @@ makePrisms ''GammaType
 makePrisms ''GammaPrimType
 makePrisms ''GammaExpr
 makePrisms ''GammaLit
+
+class GammaAnnotated t a | t -> a where
+    annotation :: Lens' t a
+    default annotation :: (Data t, Typeable a) => Lens' t a
+    annotation = singular template
+
+instance (Data t, Data a) => GammaAnnotated (GammaDecl t a) a
+instance (Data t, Data a) => GammaAnnotated (GammaBind t a) a
+instance (Data t, Data a) => GammaAnnotated (GammaStmt t a) a
+instance (Data a) => GammaAnnotated (GammaType a) a
+instance (Data t, Data a) => GammaAnnotated (GammaExpr t a) a
+
+class (HasGammaSym a) where
+    gammaSym :: Lens' a GammaSym
+
+instance (HasGammaSym GammaSym) where
+    gammaSym = id
+
+instance HasGammaSym (GammaDecl t a) where
+    gammaSym f (VarDecl a x y) = gammaSym f x <&> \x' -> VarDecl a x' y
+    gammaSym f (FunDecl a x y z w) = gammaSym f x <&> \x' -> FunDecl a x' y z w
+
+instance HasGammaSym (GammaBind t a) where
+    gammaSym f (PlainBind a x) = gammaSym f x <&> \x' -> PlainBind a x'
+    gammaSym f (TypeBind a x y) = gammaSym f x <&> \x' -> TypeBind a x' y
+

commit 8b36ffa8fe417fd28a84a917fd533e791aa55b3c

fixed mapType and freeTypeVars to work recursively on leaf types
fixes unification bug introduced in f570ed1

src/Language/Gamma/Typecheck.hs
@@ -13,6 +13,7 @@ import Data.Data
 import Data.Data.Lens
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
 
@@ -72,18 +73,26 @@ bindSym :: (Monad m) => GammaSym -> GammaType a -> TypT a m (GammaType a)
 bindSym name ty = ty <$ (bindings %= Map.insert name ty)
 
 mapType :: (GammaTypes t a) => Int -> GammaType a -> t -> t
-mapType n ty orig = orig & types %~ replace
+mapType n ty orig = orig & typeLeaves %~ replace
     where replace t@(FreeType a m) | m == n    = ty
                                    | otherwise = t
           replace t = t
 
 freeTypeVars :: (GammaTypes t a) => t -> Set Int
-freeTypeVars ty = ty ^. types._FreeType._2.to Set.singleton
+freeTypeVars ty = ty ^. typeLeaves._FreeType._2.to Set.singleton
 
-class GammaTypes t a | t -> a where
+-- a deep traversal of all non-composite types
+typeLeaves :: (GammaTypes t a) => Traversal' t (GammaType a)
+typeLeaves f t = types go t
+    where go ty | isNothing (ty ^? uniplate) = f ty
+                | otherwise                  = uniplate go ty
+
+class (Data a) => GammaTypes t a | t -> a where
+    -- a traversal of immediate type children
+    -- or self for t == GammaType
     types :: Traversal' t (GammaType a)
-    default types :: (Data t, Data a) => Traversal' t (GammaType a)
-    types = template
+    default types :: (Data t) => Traversal' t (GammaType a)
+    types = biplate
 
 instance (Data a) => GammaTypes (GammaType a) a
 instance (Data a) => GammaTypes (Map GammaSym (GammaType a)) a